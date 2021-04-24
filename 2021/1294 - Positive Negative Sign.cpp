55bfbf935b49cf1ad504fa756014bad841492993
Sahenulislam <71545997+Sahenulislam@users.noreply.github.com>
2021-04-25 02:02:41 +0600

    Add files via upload

---
+++ 2021/1294 - Positive Negative Sign.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define yes "YES"
+#define no "NO"
+#define pb push_back
+#define si size()
+#define inco " "
+#define incom "-1"
+
+int main()
+{
+    ll t,tt=1;
+    cin>>t;
+    while(t--)
+    {
+       ll n,m;
+       cin>>n>>m;
+       cout<<"Case "<<tt++<<": "<<(n*m)/2<<endl;
+    }
+    return 0;
+}
+
